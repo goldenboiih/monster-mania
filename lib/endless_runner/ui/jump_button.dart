7a513143e4915edc09133887cdd7af2f3bf9e209
Kenny <kennyla94@gmail.com>
2025-07-28 13:28:13 +0200

    Refactor jump button

--- lib/endless_runner/ui/jump_button.dart
+++ lib/endless_runner/ui/jump_button.dart
@@ -1,18 +1,19 @@
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
-import '../runner_game.dart';
+import 'package:flamegame/endless_runner/runner_game.dart';
 
 class JumpButton extends SpriteComponent
     with HasGameReference<EndlessRunnerGame>, TapCallbacks {
-  JumpButton(Sprite sprite)
+
+  JumpButton()
       : super(
       size: Vector2(128, 128),
       priority: 10,
-      sprite: sprite
   );
 
   @override
   Future<void> onLoad() async {
+    sprite = await Sprite.load('ui/jump_button.png');
     position = Vector2(game.size.x - size.x - 10, game.size.y - size.y);
   }
 
