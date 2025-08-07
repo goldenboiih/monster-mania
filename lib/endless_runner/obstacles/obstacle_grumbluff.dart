ca5b3e3b5af64ef747d2f923f61401edd9f8d9d8
Kenny <kennyla94@gmail.com>
2025-08-08 01:07:12 +0200

    Add grumbluff animation

--- lib/endless_runner/obstacles/obstacle_grumbluff.dart
+++ lib/endless_runner/obstacles/obstacle_grumbluff.dart
@@ -1,24 +1,20 @@
 import 'dart:math';
-import 'package:flame/components.dart';
+
 import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
 import 'package:flamegame/endless_runner/runner_game.dart';
-import 'package:flamegame/endless_runner/obstacles/obstacle.dart';
+
 import 'grumbluff_drop.dart';
 
 enum GrumbluffState { floatingIn, dropping, escapingRight, descending, chargingLeft }
 
-class ObstacleGrumbluff extends Obstacle
+class ObstacleGrumbluff extends SpriteAnimationGroupComponent<GrumbluffState>
     with HasGameReference<EndlessRunnerGame> {
-
   final double floatAmplitude = 6.0;
   final double floatSpeed = 4.0;
   final double floorY = 311;
   final double dropTriggerX = 500;
   final double escapeTriggerX = 600;
-
-  late Sprite idleSprite;
-  late Sprite chargingSprite;
-  late SpriteAnimation throwAnimation;
 
   late double baseY;
   double floatTime = 0.0;
@@ -28,34 +24,51 @@ class ObstacleGrumbluff extends Obstacle
 
   late Timer dropIdleTimer;
 
-  GrumbluffState state = GrumbluffState.floatingIn;
-
   ObstacleGrumbluff()
       : super(
-    size: Vector2(64, 64),
+    size: Vector2(70, 70),
     anchor: Anchor.center,
   );
 
   @override
   Future<void> onLoad() async {
     position = Vector2(game.size.x + width, game.size.y / 5);
-    sprite = await Sprite.load('grumbluff/grumbluff_32x32.png');
-    add(RectangleHitbox.relative(Vector2.all(0.7), parentSize: size));
     baseY = y;
+
+    // Load animations and static sprites
+
+    final sprite_idle = await Sprite.load('grumbluff/grumbluff_idle.png');
+    final sprite_0 = await Sprite.load('grumbluff/grumbluff_throw_0.png');
+    final sprite_1 = await Sprite.load('grumbluff/grumbluff_throw_1.png');
+    final sprite_2 = await Sprite.load('grumbluff/grumbluff_throw_2.png');
+    final dropAnimation = SpriteAnimation.spriteList([sprite_0, sprite_1, sprite_2], stepTime: 0.15);
+
+
+    // Assign animations per state
+    animations = {
+      GrumbluffState.floatingIn: SpriteAnimation.spriteList([sprite_0], stepTime: double.infinity),
+      GrumbluffState.dropping: dropAnimation,
+      GrumbluffState.escapingRight: SpriteAnimation.spriteList([sprite_idle], stepTime: double.infinity),
+      GrumbluffState.descending: SpriteAnimation.spriteList([sprite_2], stepTime: double.infinity),
+      GrumbluffState.chargingLeft: SpriteAnimation.spriteList([sprite_1], stepTime: double.infinity),
+    };
+
+    current = GrumbluffState.floatingIn;
 
     totalDrops = Random().nextInt(3) + 1;
     dropsRemaining = totalDrops;
 
     dropIdleTimer = Timer(0);
+
+    add(RectangleHitbox.relative(Vector2.all(0.7), parentSize: size));
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-
     dropIdleTimer.update(dt);
 
-    switch (state) {
+    switch (current) {
       case GrumbluffState.floatingIn:
         x -= (game.speed + 100) * dt;
         floatTime += dt;
@@ -75,14 +88,14 @@ class ObstacleGrumbluff extends Obstacle
         floatTime += dt;
         y = baseY + sin(floatTime * floatSpeed) * floatAmplitude;
         if (x >= escapeTriggerX) {
-          state = GrumbluffState.descending;
+          current = GrumbluffState.descending;
         }
         break;
 
       case GrumbluffState.descending:
         y += game.speed * dt;
         if (y >= floorY - size.y / 2) {
-          state = GrumbluffState.chargingLeft;
+          current = GrumbluffState.chargingLeft;
         }
         break;
 
@@ -92,12 +105,14 @@ class ObstacleGrumbluff extends Obstacle
           removeFromParent();
         }
         break;
+      case null:
+        // TODO: Handle this case.
+        throw UnimplementedError();
     }
   }
 
-
   void startDropping() {
-    state = GrumbluffState.dropping;
+    current = GrumbluffState.dropping;
     performDrop();
   }
 
@@ -106,14 +121,19 @@ class ObstacleGrumbluff extends Obstacle
     dropsRemaining--;
 
     if (dropsRemaining > 0) {
-      dropIdleTimer = Timer(Random().nextDouble() * 1 + 0.5, onTick: performDrop, repeat: false);
-      dropIdleTimer.start();
+      dropIdleTimer = Timer(
+        Random().nextDouble() * 1 + 0.5,
+        onTick: performDrop,
+        repeat: false,
+      )..start();
     } else {
-      // All drops done, idle briefly, then escape
-      dropIdleTimer = Timer(Random().nextDouble() * 1 + 0.5, onTick: () {
-        state = GrumbluffState.escapingRight;
-      }, repeat: false);
-      dropIdleTimer.start();
+      dropIdleTimer = Timer(
+        Random().nextDouble() * 1 + 0.5,
+        onTick: () {
+          current = GrumbluffState.escapingRight;
+        },
+        repeat: false,
+      )..start();
     }
   }
 }
