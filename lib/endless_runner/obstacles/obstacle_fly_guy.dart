24094f184e3beb986999d2d05ac4bfb998761cf0
Kenny <kennyla94@gmail.com>
2025-08-09 18:36:15 +0200

    Add different altitudes for fly guy

--- lib/endless_runner/obstacles/obstacle_fly_guy.dart
+++ lib/endless_runner/obstacles/obstacle_fly_guy.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flamegame/endless_runner/runner_game.dart';
@@ -7,12 +9,23 @@ class ObstacleFlyGuy extends SpriteAnimationComponent
   ObstacleFlyGuy()
       : super(
     size: Vector2(64, 64), // Or match the sprite's logical size
-    position: Vector2(800, 250),
     anchor: Anchor.center,
   );
 
   @override
   Future<void> onLoad() async {
+    double altitude = game.size.y - game.floorHeight - (size.y / 2) + 2;
+    final int rand = Random().nextInt(3);
+    switch (rand) {
+      case 0:
+        break;
+      case 1:
+        altitude -= 32;
+        break;
+      case 2:
+        altitude -= 64;
+    }
+    position = Vector2(game.size.x + width, altitude);
     final images = await Future.wait([
       game.images.load('fly_guy/fly_guy_down.png'),
       game.images.load('fly_guy/fly_guy_mid.png'),
