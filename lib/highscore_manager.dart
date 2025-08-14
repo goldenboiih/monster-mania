117ec6f9c33f5e3e64c4ae123b0a050488c57ef0
Kenny <kennyla94@gmail.com>
2025-08-14 13:53:24 +0200

    Remove conditional logic from saving highscore

--- lib/highscore_manager.dart
+++ lib/highscore_manager.dart
@@ -5,17 +5,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 class HighscoreManager {
   static const _prefix = 'highscore_';
 
-  // Save highscore only if it's higher than the current
   static Future<void> saveHighscore(String gameId, int score) async {
     final prefs = await SharedPreferences.getInstance();
     final key = '$_prefix$gameId';
-    final current = prefs.getInt(key) ?? 0;
-
-    if (score > current) {
-      log('set highscore to $score');
-      await prefs.setInt(key, score);
-    }
-    // await prefs.setInt(key, 0);
+    await prefs.setInt(key, score);
   }
 
   // Get highscore
